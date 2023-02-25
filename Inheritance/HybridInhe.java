6d6f6ad4ce8990da93b45c80c04341ae3a3f0d51
NityaNand <nityanandbhushan13@gmail.com>
2023-02-26 00:11:07 +0530

    changes

--- Inheritance/HybridInhe.java
+++ Inheritance/HybridInhe.java
@@ -1,5 +1,47 @@
 package Inheritance;
 
-public class HybridInhe {
+public class HybridInhe {  
+    public static void main(String args[]){ 
+
+    } 
+
     
-}
+} 
+class Animal{ 
+    void eats(){ 
+        System.out.print("They can eats"); 
+
+    }  
+    void walk(){ 
+        System.out.print("They can walk");
+    }
+
+} 
+class Birds extends Animal{ 
+    void fly(){ 
+        System.out.print("They Can fly"); 
+
+    } 
+    void wings(){ 
+        System.out.print("They hava wings");
+    }
+} 
+class Duck extends Birds { 
+    void eggs(){ 
+        System.out.print("They give Egg");
+    }  
+    void swim(){ 
+        System.out.print("They can swims");
+    } 
+
+
+} 
+class Fish extends Animal{  
+    void smalleyes(){ 
+        System.out.print("Fish hava small Eyes");
+    } 
+    void smallsize(){ 
+        System.out.print("Fish hava smallsize");
+    }
+
+} 
