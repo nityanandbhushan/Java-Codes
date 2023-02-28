54f065357cd60718df56779ab39b30fcfababee3
NityaNand <nityanandbhushan13@gmail.com>
2023-02-28 15:50:20 +0530

    changes

---
+++ PracticeQuestionOfAlpha/TrappingWater.java
@@ -0,0 +1,13 @@
+package PracticeQuestionOfAlpha;
+
+public class TrappingWater { 
+    public static void findingTrapwTer(int arr[]){  
+        
+
+    } 
+    public static void main(String args[]){  
+        int arr[]={4, 2, 0, 3, 2, 5};
+
+    }
+    
+}
