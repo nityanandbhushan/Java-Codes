18ef052f284d67051ce80d62e75f14f1c20c0375
NityaNand <nityanandbhushan13@gmail.com>
2023-03-11 21:26:53 +0530

    changes

---
+++ PracticeQuestionOfAlpha/CalcuTranspose.java
@@ -0,0 +1,14 @@
+package PracticeQuestionOfAlpha;
+
+public class CalcuTranspose { 
+    public static void transpose(int arr[][]){ 
+        for(int i=0;i<arr.len)
+
+
+    } 
+    public static void main(String args[]){ 
+        int array[][]={{11,10,9},{6,4,2}}; 
+        transpose(array);
+    }
+    
+}
