b6ff03c16338b365cb98744783c75c50d43b2982
NityaNand <nityanandbhushan13@gmail.com>
2023-03-15 01:59:25 +0530

    changes

---
+++ PatternInJavaPractice/StarPatt2.java
@@ -0,0 +1,17 @@
+package PatternInJavaPractice;
+
+public class StarPatt2 {
+    public static void main(String args[]) 
+    { 
+        for(int line=4;line>0;line--) 
+        { 
+            for(int star=1;star<=line;star++) 
+            { 
+                System.out.print("*");
+            }  
+            System.out.println(" ");
+
+    }
+    
+}
+}
