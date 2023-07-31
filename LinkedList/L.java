544807b65add1b47ef61e024b504e83965350f7c
NityaNand <nityanandbhushan13@gmail.com>
2023-07-31 15:32:09 +0530

    changes

---
+++ LinkedList/L.java
@@ -0,0 +1,19 @@
+package LinkedList;
+
+public class L {
+    public static void main(String args[]) {
+        int arr[] = { 64, 25, 12, 22, 11 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
